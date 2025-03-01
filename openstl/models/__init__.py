d92ae1cfbe1fe8c896e7db0322f527e9ea26152a
rolson24 <raif.t.olson.24@dartmouth.edu>
2025-03-01 18:51:10 -0500

    fix

--- openstl/models/__init__.py
+++ openstl/models/__init__.py
@@ -8,7 +8,7 @@ from .phydnet_model import PhyDNet_Model
 from .predrnn_model import PredRNN_Model
 from .predrnnpp_model import PredRNNpp_Model
 from .predrnnv2_model import PredRNNv2_Model
-from .simvp_model import SimVP_Model, Encoder, Decoder, MidIncepNet, MidMetaNe
+from .simvp_model import SimVP_Model, Encoder, Decoder, MidIncepNet, MidMetaNet
 from .mmvp_model import MMVP_Model
 from .swinlstm_model import SwinLSTM_D_Model, SwinLSTM_B_Model
 from .simple_jepa_model import SimpleJEPA_Model
@@ -18,7 +18,7 @@ from .complicated_JEPA_model import Complicated_JEPA_Model
 __all__ = [
     'ConvLSTM_Model', 'E3DLSTM_Model', 'MAU_Model', 'MIM_Model', 'PhyDNet_Model',
     'PredRNN_Model', 'PredRNNpp_Model', 'PredRNNv2_Model', 'SimVP_Model',
-    'Encoder', 'Decoder', 'MidIncepNet', 'MidMetaNe',
+    'Encoder', 'Decoder', 'MidIncepNet', 'MidMetaNet',
     "MMVP_Model", 'SwinLSTM_D_Model', 'SwinLSTM_B_Model', 'SimpleJEPA_Model', 
     'JEPA_Mamba_Model', 'Complicated_JEPA_Model'
 ]
