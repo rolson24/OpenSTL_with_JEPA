759ccafd01c69a73d22aa0fa47d74922a4540551
rolson24 <raif.t.olson.24@dartmouth.edu>
2025-03-02 00:42:16 -0500

    update config

--- configs/bouncing_shapes/ComplicatedJEPA.py
+++ configs/bouncing_shapes/ComplicatedJEPA.py
@@ -10,10 +10,10 @@ lrt_decoder = 0.01
 aft_seq_length = 10
 pre_seq_length = 10
 total_length = 20
-hid_S = 18
+hid_S = 20
 hid_T = 128
 N_T = 4
-N_S = 4
+N_S = 8
 # training
 lr = 1e-4
 batch_size = 16
