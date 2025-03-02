059acadd18b89e11b181781df9dafc5ff640ed44
rolson24 <raif.t.olson.24@dartmouth.edu>
2025-03-02 00:05:36 -0500

    fix dataloader

--- openstl/datasets/dataloader_bouncing_shapes.py
+++ openstl/datasets/dataloader_bouncing_shapes.py
@@ -114,11 +114,11 @@ def load_data(batch_size, val_batch_size, data_root, num_workers=4, data_name='b
               distributed=False, use_augment=False, use_prefetcher=False, drop_last=False):
 
     image_size = in_shape[-1] if in_shape is not None else 64
-    train_set = BouncingShapes(root=data_root, is_train=True, data_name=data_name,
+    train_set = BouncingShapesDataset(root=data_root, is_train=True, data_name=data_name,
                             n_frames_input=pre_seq_length,
                             n_frames_output=aft_seq_length, num_objects=[1],
                             image_size=image_size, use_augment=use_augment)
-    test_set = BouncingShapes(root=data_root, is_train=False, data_name=data_name,
+    test_set = BouncingShapesDataset(root=data_root, is_train=False, data_name=data_name,
                            n_frames_input=pre_seq_length,
                            n_frames_output=aft_seq_length, num_objects=[1],
                            image_size=image_size, use_augment=False)
