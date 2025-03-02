2b5211a2d0d1c615cb6b77d75a109e4eac8da313
rolson24 <raif.t.olson.24@dartmouth.edu>
2025-03-02 00:57:16 -0500

    update complicated to defaults

--- configs/bouncing_shapes/ComplicatedJEPA.py
+++ configs/bouncing_shapes/ComplicatedJEPA.py
@@ -10,10 +10,10 @@ lrt_decoder = 0.01
 aft_seq_length = 10
 pre_seq_length = 10
 total_length = 20
-hid_S = 64
-hid_T = 32
-N_T = 4
-N_S = 8
+# hid_S = 64
+# hid_T = 32
+# N_T = 4
+# N_S = 8
 # training
 lr = 1e-4
 batch_size = 16
