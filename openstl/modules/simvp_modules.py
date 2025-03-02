c527f356e3e37a05fae925720c29c60b6db95805
rolson24 <raif.t.olson.24@dartmouth.edu>
2025-03-01 19:08:40 -0500

    print

--- openstl/modules/simvp_modules.py
+++ openstl/modules/simvp_modules.py
@@ -443,6 +443,7 @@ class SwinSubBlock(SwinTransformerBlock):
         B, C, H, W = x.shape
         print("SwinSubBlock x shape:", x.shape)
         x = x.flatten(2).transpose(1, 2)
+        print("SwinSubBlock reshaped x shape:", x.shape)
         x = self.norm1(x)
         x = x.view(B, H, W, C)
         x = super().forward(x)
