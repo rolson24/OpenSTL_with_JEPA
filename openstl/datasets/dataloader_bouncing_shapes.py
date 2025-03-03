5d91bb21cb85f25641fa3f7a5a1a92ebb0d8bebd
rolson24 <raif.t.olson.24@dartmouth.edu>
2025-03-02 22:10:48 -0500

    update dataloader

--- openstl/datasets/dataloader_bouncing_shapes.py
+++ openstl/datasets/dataloader_bouncing_shapes.py
@@ -98,7 +98,7 @@ class BouncingShapesDataset(Dataset):
         input = torch.from_numpy(input / 255.0).contiguous().float()
 
         # map the 0th label to be 'triangle'->0, 'circle'->1, 'rectangle'->2
-        labels = np.array([0 if x == 'triangle' else 1 if x == 'circle' else 2 for x in labels])
+        labels = np.array([0 if x[0] == 'triangle' else 1 if x[0] == 'circle' else 2 for x in labels])
 
         labels = torch.from_numpy(labels)
 
