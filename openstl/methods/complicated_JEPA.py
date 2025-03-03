1e46dfb0658903c1fbbfa2593ce83b9b1eb23494
rolson24 <raif.t.olson.24@dartmouth.edu>
2025-03-02 23:41:43 -0500

    fix loader

--- openstl/methods/complicated_JEPA.py
+++ openstl/methods/complicated_JEPA.py
@@ -36,6 +36,7 @@ class Bounching_Shapes_Complicated_JEPA(Base_method):
 
     def load_state_dict(self, checkpoint, strict):
         # Load the state dictionary with strict=False
+        checkpoint = torch.load(checkpoint)
         self.model.load_state_dict(checkpoint['state_dict'], strict=False)
 
     def forward(self, batch_x, batch_y, **kwargs):
