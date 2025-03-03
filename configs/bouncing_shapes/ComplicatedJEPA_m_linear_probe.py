8675c2c4ce7272472df990cc8494f02a5a90fafc
rolson24 <raif.t.olson.24@dartmouth.edu>
2025-03-02 21:55:34 -0500

    add config for linear probing

---
+++ configs/bouncing_shapes/ComplicatedJEPA_m_linear_probe.py
@@ -0,0 +1,22 @@
+method = 'complicated_jepa'
+# model
+in_shape = [10, 1, 64, 64]
+embed_dim = 128
+
+alpha = 0.1
+beta = 0.1
+train_decoder = True
+lrt_decoder = 0.01
+aft_seq_length = 10
+pre_seq_length = 10
+total_length = 20
+hid_S = 64
+hid_T = 32
+N_T = 4
+N_S = 8
+# training
+lr = 1e-4
+batch_size = 16
+sched = 'onecycle'
+epoch = 100
+train_linear_probe = True
